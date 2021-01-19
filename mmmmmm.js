07f0f7c56a81b21b98a5af0bf7c7142fcce3b9b7
Mohammad_Hosein <mhjmail@gmail.com>
2021-01-20 01:07:57 +0330

    Add files via upload

---
+++ mmmmmm.js
@@ -0,0 +1,13 @@
+// بسم الله الرحمن الرحیم
+// <script>
+
+function thisFunction(){
+	return "hhhhhhhhhhhhhh";
+}
+
+MyObject = {} ;
+MyObject.MyFunction  = thisFunction;
+
+b = 88888;
+c = 1111111111111;
+// </script>
